254ac8a2a24a9df63644d160e144351e5e1af39a
Okpara007 <chinemeremokpara93@gmail.com>
2024-08-13 12:39:50 +0100

    updated allowed host

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-3g8-7qxxm0bvjkswf4f5^_a4yno9fqn&t+8@rommgdkp_11&1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://blog-2di2.onrender.com']
-
+ALLOWED_HOSTS = ['blog-2di2.onrender.com']
 
 # Application definition
 
