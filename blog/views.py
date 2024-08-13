228c0a6d1da9ea74f8bbaf935c41359a071b1408
Okpara007 <chinemeremokpara93@gmail.com>
2024-08-13 12:50:50 +0100

    updated code

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the Blog API. Go to /graphql to interact with the API.")
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,8 +1,11 @@
+# in blog_project/urls.py
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
+from blog.views import home  # Import the home view
 
 urlpatterns = [
+    path("", home, name="home"),  # Add this line for the root URL
     path("graphql", GraphQLView.as_view(graphiql=True)),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
